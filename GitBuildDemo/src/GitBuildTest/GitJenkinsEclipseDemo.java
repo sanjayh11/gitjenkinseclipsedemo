4ab4a9265c69e001fbd1a0eebbb3b2d9c72e8fe7
sanja <sanja@192.168.1.3>
2018-01-19 15:59:35 +0530

    commit 1 - Sanjay

---
+++ GitBuildDemo/src/GitBuildTest/GitJenkinsEclipseDemo.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package GitBuildTest;
+
+/**
+ * @author sanja
+ *
+ */
+public class GitJenkinsEclipseDemo {
+
+	/**
+	 * 
+	 */
+	public GitJenkinsEclipseDemo() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    System.out.println ("I am in the main method - rebuild 1");
+	}
+
+}
